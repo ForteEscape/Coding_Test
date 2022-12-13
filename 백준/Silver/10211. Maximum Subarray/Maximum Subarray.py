cc1e66988ab8cca49e97e71e1e5d3e4aebac37fe
sehunkim <sehun5515@naver.com>
2022-12-13 14:06:01 +0900

    [Silver IV] Title: Maximum Subarray, Time: 116 ms, Memory: 114488 KB -BaekjoonHub

---
+++ 백준/Silver/10211. Maximum Subarray/Maximum Subarray.py
@@ -0,0 +1,13 @@
+T = int(input())
+
+for _ in range(T):
+    N = int(input())
+    data = list(map(int, input().split()))
+
+    dp = [0] * N
+    dp[0] = data[0]
+
+    for i in range(1, N):
+        dp[i] = max(dp[i - 1] + data[i], data[i])
+
+    print(max(dp))
