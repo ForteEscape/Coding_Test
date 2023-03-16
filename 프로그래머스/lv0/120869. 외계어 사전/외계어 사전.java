6c000784cf112c557bc4f3d5e97ce02ea3627538
sehunkim <sehun5515@naver.com>
2023-03-16 17:58:12 +0900

    [level 0] Title: 외계어 사전, Time: 0.03 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.java
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.java
@@ -1,37 +1,20 @@
 class Solution {
     public int solution(String[] spell, String[] dic) {
         int answer = 0;
-        int[] result = new int[2];
         
         for(String data: dic){
-            int[] alphabet = new int[26];
             boolean flag = true;
             
-            if(data.length() > spell.length){
-                result[1]++;
-                continue;
-            }
-            
-            for(int i = 0; i < data.length(); i++){
-                alphabet[data.charAt(i) - 'a']++;
-            }
-            
-            for(int i = 0; i < spell.length; i++){
-                if(alphabet[spell[i].charAt(0) - 'a'] != 1){
-                    flag = false;
-                    break;
+            if(data.length() == spell.length){
+                for(String element: spell){
+                    if(!data.contains(element)){
+                        flag = false;
+                        break;
+                    }
                 }
-            }
-            
-            if(flag){
-                result[0]++;
-            } else{
-                result[1]++;
-            }
+                if(flag) return 1;
+            } 
         }
-        
-        answer = result[0] == 0 ? 2 : 1;
-        
-        return answer;
+        return 2;
     }
 }
