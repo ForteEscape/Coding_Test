16b01bf8e85bb86a61afd12c1451e9394422ca77
sehunkim <sehun5515@naver.com>
2023-04-02 16:10:54 +0900

    [level 1] Title: 폰켓몬, Time: 3.16 ms, Memory: 81.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < nums.length; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+        
+        if(map.size() >= nums.length / 2){
+            return nums.length / 2;
+        } else{
+            return map.size();
+        }
+    }
+}
