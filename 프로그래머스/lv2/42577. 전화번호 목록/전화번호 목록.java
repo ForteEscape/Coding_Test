f4c63f23d3540e540aa1d39a002a6e1af086e931
sehunkim <sehun5515@naver.com>
2023-04-10 14:24:09 +0900

    [level 2] Title: 전화번호 목록, Time: 179.02 ms, Memory: 135 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        HashSet<String> prefix = new HashSet<>();
+        for(int i = 0; i < phone_book.length; i++){
+            prefix.add(phone_book[i]);
+        }
+        
+        for(int i = 0; i < phone_book.length; i++){
+            for(int j = 0; j < phone_book[i].length(); j++){
+                if(prefix.contains(phone_book[i].substring(0, j))) return false;
+            }
+        }
+        
+        return true;
+    }
+}
