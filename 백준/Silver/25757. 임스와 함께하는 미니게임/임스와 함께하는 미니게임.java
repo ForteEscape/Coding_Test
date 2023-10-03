bd1961e6a559392bb1eba8acae1c03b886bb9af4
sehunkim <sehun5515@naver.com>
2023-10-03 15:34:36 +0900

    [Silver V] Title: 임스와 함께하는 미니게임, Time: 276 ms, Memory: 27144 KB -BaekjoonHub

---
+++ 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    int N = Integer.parseInt(st.nextToken());
+    String gameType = st.nextToken();
+
+    Set<String> set = new HashSet<>();
+
+    for (int i = 0; i < N; i++){
+      set.add(br.readLine());
+    }
+
+    if (gameType.equals("Y")){
+      System.out.println(set.size());
+    } else if (gameType.equals("F")){
+      System.out.println(set.size() / 2);
+    } else if (gameType.equals("O")){
+      System.out.println(set.size() / 3);
+    }
+  }
+}
