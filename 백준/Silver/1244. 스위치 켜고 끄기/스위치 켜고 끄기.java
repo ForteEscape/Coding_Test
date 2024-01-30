e50d27008fb5ef5a05b0f8ab280c0d58e982e2a1
sehunkim <sehun5515@naver.com>
2024-01-30 17:32:21 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 128 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -1,69 +1,62 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
 
-  static int[] switchArr;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] switchData = new int[N + 1];
+		for(int i = 1; i <= N; i++) {
+			switchData[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int M = Integer.parseInt(br.readLine());
+		for(int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int gender = Integer.parseInt(st.nextToken());
+			int num = Integer.parseInt(st.nextToken());
+			
+			if(gender == 1) {
+				for(int j = 1 * num; j <= N; j += num) {
+					switchData[j] = 1 - switchData[j];
+				}
+			} else {
+				for(int j = 1;; j++) {
+					int left = num - j;
+					int right = num + j;
+					
+					if(left <= 0 || right > N) {
+						break;
+					}
+					
+					if(switchData[left] != switchData[right]) {
+						break;
+					}
+					
+					switchData[left] = 1 - switchData[left];
+					switchData[right] = 1 - switchData[right];
+				}
+				
+				switchData[num] = 1 - switchData[num];
+			}
+			
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i = 1; i <= N; i++ ) {
+			if(i % 20 == 0) {
+				sb.append(switchData[i]).append("\n");
+			} else {
+				sb.append(switchData[i]).append(" ");
+			}
+		}
+		System.out.println(sb);
+	}
 
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-    int N = Integer.parseInt(br.readLine());
-    switchArr = new int[N + 1];
-
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    int idx = 1;
-
-    while (st.hasMoreTokens()) {
-      switchArr[idx++] = Integer.parseInt(st.nextToken());
-    }
-
-    int studentNum = Integer.parseInt(br.readLine());
-
-    for (int i = 0; i < studentNum; i++) {
-      st = new StringTokenizer(br.readLine());
-
-      int gender = Integer.parseInt(st.nextToken());
-      int startIdx = Integer.parseInt(st.nextToken());
-
-      changeStatus(gender, startIdx);
-    }
-
-    for (int i = 1; i < switchArr.length; i++){
-      System.out.print(switchArr[i] + " ");
-
-      if (i % 20 == 0) {
-        System.out.println();
-      }
-    }
-  }
-
-  public static void changeStatus(int gender, int startIdx) {
-    if (gender == 1) {
-      for (int i = startIdx; i < switchArr.length; i += startIdx) {
-        switchArr[i] = 1 - switchArr[i];
-      }
-    } else {
-
-      int left = startIdx - 1;
-      int right = startIdx + 1;
-      switchArr[startIdx] = 1 - switchArr[startIdx];
-
-      while (true) {
-        if (left <= 0 || left >= switchArr.length || right <= 0 || right >= switchArr.length ||
-            switchArr[left] != switchArr[right]
-        ) {
-          break;
-        }
-
-        switchArr[left] = 1 - switchArr[left];
-        switchArr[right] = 1 - switchArr[right];
-
-        left--;
-        right++;
-      }
-    }
-  }
+	
 }
