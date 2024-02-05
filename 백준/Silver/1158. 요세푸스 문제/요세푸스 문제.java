f75cfe8f858483be279c9e665b821b3162f6bcbd
sehunkim <sehun5515@naver.com>
2024-02-05 16:12:58 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 332 ms, Memory: 14896 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,38 +1,40 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-
-        LinkedList<Integer> list = new LinkedList<>();
-
-        for (int i = 1; i <= N; i++){
-            list.add(i);
-        }
-
-        StringBuilder sb = new StringBuilder();
-        sb.append("<");
-
-        while(!list.isEmpty()){
-            for(int i = 0; i < K - 1; i++){
-                list.addLast(list.pollFirst());
-            }
-
-            sb.append(list.pollFirst());
-            if (!list.isEmpty()){
-                sb.append(", ");
-            } else{
-                sb.append(">");
-            }
-        }
-
-        System.out.println(sb.toString());
-    }
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		
+		Deque<Integer> queue = new ArrayDeque<>();
+		for(int i = 1; i <= N; i++) {
+			queue.addLast(i);
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append("<");
+		int idx = 1;
+		while(!queue.isEmpty()) {
+			if(idx % K == 0) {
+				if(queue.size() == 1) {
+					sb.append(queue.pollFirst());
+				} else {
+					sb.append(queue.pollFirst()).append(", ");
+				}
+			} else {
+				queue.addLast(queue.pollFirst());
+			}
+			
+			idx++;
+		}
+		sb.append(">");
+		System.out.println(sb);
+	}
 }
