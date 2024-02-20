16e3b3f55ce71256538b72c5b6a8308cc468d88d
sehunkim <sehun5515@naver.com>
2024-02-20 13:40:25 +0900

    [Gold V] Title: 적록색약, Time: 168 ms, Memory: 16232 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,97 +1,108 @@
+import java.awt.Point;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
-//8 directions
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
 public class Main {
-	
-	static class Location {
-		int y;
-		int x;
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    
+    static int N;
+    static char[][] map;
+    static int[] dx = {0,0,1,-1};
+    static int[] dy = {1,-1,0,0};
+    static boolean[][] visit;    
+    
+    public static boolean isInside(int x, int y) {
+    	if(x>=0 && x<N && y>=0 && y<N) return true;
+    	return false;
+    }
+    
+    public static boolean isSameColor(char a, char b) {
+    	if(a == b) return true;
+    	if(a == 'R' && b == 'G') return true;
+    	if(a == 'G' && b == 'R') return true;
+    	return false;
+    }
+    
+    public static void redGreenBlindnessDfs(int x, int y, char nowColor) {
+    	visit[x][y] = true;
+    	
+    	for(int i=0;i<4;i++) {
+    		int nx = x+dx[i];
+    		int ny = y+dy[i];
+    		
+    		if(isInside(nx,ny) && !visit[nx][ny] && isSameColor(nowColor, map[nx][ny])) {
+    			redGreenBlindnessDfs(nx,ny, map[nx][ny]);
+    		}
+    	}
+    }
+    
+    public static void dfs(int x, int y, char nowColor) {
+    	visit[x][y] = true;
+    	
+    	for(int i=0;i<4;i++) {
+    		int nx = x+dx[i];
+    		int ny = y+dy[i];
+    		
+    		if(isInside(nx,ny) && !visit[nx][ny] && nowColor == map[nx][ny]) {
+    			dfs(nx,ny, map[nx][ny]);
+    		}
+    	}
+    }
+
+	public static void main(String[] args) throws Exception {
 		
-		Location(int y, int x) {
-			this.y = y;
-			this.x = x;
+		int cnt, colorCnt;
+		cnt = colorCnt = 0;
+		N = Integer.parseInt(br.readLine());		
+		
+		map = new char[N][N];
+		visit = new boolean[N][N];
+		
+		for(int i=0;i<N;i++) {
+			String s = br.readLine();
+			for(int j=0;j<N;j++) {
+				map[i][j] = s.charAt(j);
+			}
+		}		
+		
+		for(int i=0;i<N;i++) {
+			for(int j=0;j<N;j++) {
+				if(!visit[i][j]) { 
+					dfs(i,j,map[i][j]);
+					cnt++;
+				}				
+			}
 		}
+
+		visit = new boolean[N][N];
+		
+		for(int i=0;i<N;i++) {
+			for(int j=0;j<N;j++) {
+				if(!visit[i][j]) { 
+					redGreenBlindnessDfs(i,j,map[i][j]);
+					colorCnt++;
+				}	
+			}
+		}
+		
+		bw.write(cnt+" "+colorCnt);
+		bw.close();
 	}
 
-	static int N;
-	static char[][] board;
-	static char[][] board2;
-	static int[] dy = {-1, 0, 1, 0};
-	static int[] dx = {0, 1, 0, -1};
-	static boolean[][] visited;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		
-		board = new char[N][N];
-		board2 = new char[N][N];
-		visited = new boolean[N][N];
-		
-		for(int i = 0; i < N; i++) {
-			String line = br.readLine();
-			for(int j = 0; j < N; j++) {
-				board[i][j] = line.charAt(j);
-				
-				if(board[i][j] == 'G') {
-					board2[i][j] = 'R';
-				} else {
-					board2[i][j] = board[i][j];
-				}
-			}
-		}
-		
-		int ans = 0;
-		for(int i = 0; i<  N; i++) {
-			for(int j = 0; j < N; j++) {
-				if(!visited[i][j]) {
-					visited[i][j] = true;
-					ans++;
-					bfs(i, j, board);
-				}
-			}
-		}
-		
-		visited = new boolean[N][N];
-		int ans2 = 0;
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < N; j++) {
-				if(!visited[i][j]) {
-					visited[i][j] = true;
-					ans2++;
-					bfs(i, j, board2);
-				}
-			}
-		}
-		
-		System.out.println(ans + " " + ans2);
-	}
-	
-	public static void bfs(int y, int x, char[][] board) {
-		Deque<Location> queue = new ArrayDeque<>();
-		queue.addLast(new Location(y, x));
-		
-		while(!queue.isEmpty()) {
-			Location cur = queue.pollFirst();
-			
-			for(int i = 0; i < 4; i++) {
-				int ny = cur.y + dy[i];
-				int nx = cur.x + dx[i];
-				
-				if(isUnreachable(ny, nx) || visited[ny][nx] || board[cur.y][cur.x] != board[ny][nx]) {
-					continue;
-				}
-				
-				visited[ny][nx] = true;
-				queue.addLast(new Location(ny, nx));
-			}
-		}
-	}
-	
-	public static boolean isUnreachable(int y, int x) {
-		return y < 0 || y >= N || x < 0 || x >= N;
-	}
 }
