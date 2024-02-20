8a71293e459932ce577dcfb84e6af681fe338eac
sehunkim <sehun5515@naver.com>
2024-02-20 13:37:33 +0900

    [Gold III] Title: 줄 세우기, Time: 472 ms, Memory: 44464 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,65 +1,64 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+import java.util.StringTokenizer;
+//8 directions
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-
-        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-
-        for (int i = 0; i < N + 1; i++){
-            graph.add(new ArrayList<>());
-        }
-
-        int[] inDegree = new int[N + 1];
-
-        // graph construction
-        for (int i = 0; i < M; i++){
-            st = new StringTokenizer(br.readLine());
-
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
-
-            inDegree[to]++;
-            graph.get(from).add(to);
-        }
-
-        Deque<Integer> queue = new ArrayDeque<>();
-        for (int i = 1; i < N + 1; i++){
-            if (inDegree[i] == 0){
-                queue.addLast(i);
-            }
-        }
-
-        int[] answer = new int[N];
-        int idx = 0;
-
-        while(!queue.isEmpty()){
-            int cur = queue.pollFirst();
-
-            answer[idx++] = cur;
-
-            for (int i = 0; i < graph.get(cur).size(); i++){
-                int adj = graph.get(cur).get(i);
-
-                inDegree[adj]--;
-                if (inDegree[adj] == 0){
-                    queue.addLast(adj);
-                }
-            }
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for (int element: answer){
-            sb.append(element).append(" ");
-        }
-
-        System.out.println(sb.toString());
-    }
+	
+	static int[] indegree;
+	static List<Integer>[] graph;
+	static int V, E;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		V = Integer.parseInt(st.nextToken());
+		E = Integer.parseInt(st.nextToken());
+		
+		graph = new List[V + 1];
+		indegree = new int[V + 1];
+		for(int i = 1; i <= V; i++) {
+			graph[i] = new ArrayList<>();
+		}
+		
+		for(int i = 0; i< E; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			
+			graph[from].add(to);
+			indegree[to]++;
+		}
+		
+		Deque<Integer> queue = new ArrayDeque<>();
+		StringBuilder sb = new StringBuilder();
+		 
+		for(int i = 1; i <= V; i++) {
+			if(indegree[i] == 0) {
+				sb.append(i).append(" ");
+				queue.addLast(i);
+			}
+		}
+		
+		while(!queue.isEmpty()) {
+			int cur = queue.pollFirst();
+			
+			for(int adjNode : graph[cur]) {
+				indegree[adjNode]--;
+				
+				if(indegree[adjNode] == 0) {
+					sb.append(adjNode).append(" ");
+					queue.addLast(adjNode);
+				}
+			}
+		}
+		
+		System.out.println(sb.toString());
+	}
 }
