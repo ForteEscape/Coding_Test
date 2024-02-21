d158f61629eae11910ae0091d15a6a4258a39370
sehunkim <sehun5515@naver.com>
2024-02-21 16:18:29 +0900

    [Gold V] Title: 암호 만들기, Time: 200 ms, Memory: 18808 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -1,63 +1,90 @@
-import java.io.*;
-import java.util.*;
+import java.awt.Point;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+import javax.print.StreamPrintService;
+
+/**
+ * 
+ */
 
 public class Main {
-	
-	static int L, C;
-	static char[] candidate;
-	static Set<Character> mo;
-	
-	static {
-		mo = new HashSet<>();
-		mo.addAll(Arrays.asList('a', 'e', 'i', 'o', 'u'));
-	}
-	
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static int L,C;
+    static char[] arr;
+    static boolean[] visit;
+    
+    public static boolean isMo(char c) {
+    	if(c == 'a' || c == 'e' || c=='i' || c=='o' || c=='u') return true;
+    	return false;
+    }
+    
+    public static void dfs(int index, int moCnt, int zaCnt, String str) throws Exception {
+    	//System.out.println(str);
+    	if(str.length() >= L) {
+    		if(moCnt>=1 && zaCnt>=2) {
+    			bw.write(str+"\n");
+    		}
+    		return;
+    	}
+    	//visit[index] = true;
+    	for(int i=index;i<C;i++) {
+    		if(!visit[i]) {
+	    		visit[i] = true;
+	    		if(isMo(arr[i])) dfs(i+1, moCnt+1, zaCnt, str+arr[i]);
+	    		else dfs(i+1, moCnt, zaCnt+1, str+arr[i]);
+	    		visit[i] = false;
+    		}
+    	}
+    }
+
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
+		st = new StringTokenizer(br.readLine(), " ");
 		L = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
 		
-		candidate = new char[C];
-		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < C; i++) {
-			candidate[i] = st.nextToken().charAt(0);
+		arr = new char[C];
+		visit = new boolean[C];
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		
+		for(int i=0;i<C;i++) {
+			arr[i] = st.nextToken().charAt(0);
 		}
 		
-		Arrays.sort(candidate);
+		Arrays.sort(arr);
+		//System.out.println(Arrays.toString(arr));
+		dfs(0,0,0,"");
 		
-		solve(0, new StringBuilder(), L);
-	}
-	
-	public static void solve(int selected, StringBuilder sb, int r) {
-		if(r == 0) {
-			if(sb.length() < 3) return;
-			String result = sb.toString();
-			int cnt = 0;
-			
-			for(int i = 0; i < result.length(); i++) {
-				if(mo.contains(result.charAt(i))) {
-					cnt++;
-				}
-			}
-			
-			if(cnt == 0 || result.length() - cnt < 2) {
-				return;
-			}
-			
-			System.out.println(sb.toString());
-			return;
-		}
-		
-		if(selected == C) {
-			return;
-		}
-		
-		sb.append(candidate[selected]);
-		solve(selected + 1, sb, r - 1);
-		sb.deleteCharAt(sb.length() - 1);
-		solve(selected + 1, sb, r);
-		
-	}
+		bw.write("");
+
+		bw.close();
+	}	
+
 }
+
+/**
+4 6
+0 0 0 0 0 0
+3 0 0 0 0 0
+0 0 1 0 6 0
+0 0 0 0 0 0
+13
+
+*/
