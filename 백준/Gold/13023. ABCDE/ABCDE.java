4b6d5e898bb28ef793a1a4826fe853a057309b43
sehunkim <sehun5515@naver.com>
2024-02-21 13:54:59 +0900

    [Gold V] Title: ABCDE, Time: 288 ms, Memory: 16068 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.java
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -1,63 +1,98 @@
-import java.io.*;
-import java.util.*;
+import java.awt.Point;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+/**
+ * 
+ */
 
 public class Main {
-	
-	static List<Integer>[] graph;
-	static int V, E;
-	static boolean[] visited;
-	static boolean flag;
-
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static boolean[] visit;
+    static ArrayList<Integer>[] G;
+    
+    static int N, M;    
+    static boolean isExist = false;
+    
+    public static void dfs(int index, int cnt) {
+    	if(cnt >= 4) { //친구 관계 만족!
+    		isExist = true;
+    		return;
+    	}    	
+		visit[index] = true;  
+    	for(int i=0;i<G[index].size();i++) {
+    		if(isExist) return;
+    		int nextNode = G[index].get(i);
+    		if(!visit[nextNode]) {
+    			dfs(nextNode, cnt+1);
+    			visit[nextNode] = false;
+    		}
+    	}
+    	
+    }
+    
+    
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 		
-		V = Integer.parseInt(st.nextToken());
-		E = Integer.parseInt(st.nextToken());
+		G = new ArrayList[N];
+		visit = new boolean[N];
 		
-		graph = new List[V];
-		for(int i = 0; i < V; i++) {
-			graph[i] = new ArrayList<>();
+		for(int i=0;i<N;i++) {
+			G[i] = new ArrayList<Integer>();
 		}
 		
-		for(int i = 0; i < E; i++) {
-			st = new StringTokenizer(br.readLine());
+		for(int i=0;i<M;i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
 			
-			int from = Integer.parseInt(st.nextToken());
-			int to = Integer.parseInt(st.nextToken());
-			
-			graph[from].add(to);
-			graph[to].add(from);
+			G[a].add(b);
+			G[b].add(a);
 		}
 		
-		for(int i = 0; i < V; i++) {
-			visited = new boolean[V];
-			flag = false;
-			dfs(i, 1);
-			
-			if(flag) {
-				System.out.println(1);
-				return;
-			}
+		for(int i=0;i<N;i++) {
+			if(isExist) break;
+			Arrays.fill(visit, false);
+			dfs(i,0);
 		}
+
+		if(isExist) bw.write("1");
+		else bw.write("0");
+
 		
-		System.out.println(0);
-	}
-	
-	public static void dfs(int startNode, int depth) {
-		if(depth == 5) {
-			flag = true;
-			return;
-		}
 		
-		visited[startNode] = true;
-		
-		for(int adjNode : graph[startNode]) {
-			if(!visited[adjNode]) {
-				visited[adjNode] = true;
-				dfs(adjNode, depth + 1);
-				visited[adjNode] = false;
-			}
-		}
-	}
+
+		bw.close();
+	}	
+
 }
+
+/**
+
+6 6
+0 1
+0 2
+0 3
+0 4
+0 5
+5 1
+*/
