823724944bcf14e4d1466558389faf62c503cb2c
sehunkim <sehun5515@naver.com>
2024-02-27 13:23:26 +0900

    [Gold IV] Title: 최단경로, Time: 820 ms, Memory: 108988 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,76 +1,108 @@
+import java.awt.Point;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+import javax.print.StreamPrintService;
+
 
 public class Main {
-    static class Node implements Comparable<Node>{
-        int to;
-        int weight;
+	
+	/**
+	 * dp[0] : 0
+	 * dp[1] : 2
+	 * dp[2] : 5
+	 * dp[3] : dp[1] + dp[2]*2 = 2+10 = 12
+	 */
+	
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static int V,E,K;
+    static int[] dis = new int[20002];
+    static boolean[] visited = new boolean[20002];
+    static ArrayList<Point>[] G = new ArrayList[20002];
+    
+    static void BFS() {        
+        PriorityQueue<Point> pq = new PriorityQueue<>(new Comparator<Point>() {
 
-        public Node(int to ,int weight){
-            this.to = to;
-            this.weight = weight;
-        }
+			@Override
+			public int compare(Point o1, Point o2) {
+				return o1.x - o2.x;
+			}
+		}); //가중치, 정점 오름차순 정렬
+        pq.offer(new Point(0, K)); //처음
 
-        @Override
-        public int compareTo(Node o) {
-            return this.weight - o.weight;
-        }
-    }
+        while (!pq.isEmpty()) {
+            int w = pq.peek().x;
+            int v = pq.peek().y;
+            pq.poll();
+            
+            visited[v] = true;
 
-    final static int INF = 10000000;
+            for (int i = 0; i < G[v].size(); i++) {
+                int next = G[v].get(i).x;
+                int nextW = G[v].get(i).y;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int V = Integer.parseInt(st.nextToken());
-        int E = Integer.parseInt(st.nextToken());
-        int startNode = Integer.parseInt(br.readLine());
-
-        ArrayList<ArrayList<Node>> graph = new ArrayList<>();
-        int[] dist = new int[V + 1];
-
-        for (int i = 0; i < V + 1; i++){
-            graph.add(new ArrayList<>());
-            dist[i] = INF;
-        }
-
-        for (int i = 0; i < E; i++){
-            st = new StringTokenizer(br.readLine());
-
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-
-            graph.get(u).add(new Node(v, w));
-        }
-
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        pq.offer(new Node(startNode, 0));
-        dist[startNode] = 0;
-
-        while(!pq.isEmpty()){
-            Node cur = pq.poll();
-
-            for (int i = 0; i < graph.get(cur.to).size(); i++){
-                Node adjNode = graph.get(cur.to).get(i);
-
-                if (dist[adjNode.to] > cur.weight + adjNode.weight){
-                    dist[adjNode.to] = cur.weight + adjNode.weight;
-                    pq.offer(new Node(adjNode.to, dist[adjNode.to]));
+                if (w + nextW < dis[next]) { //최단경로 수정
+                    dis[next] = w + nextW;
+                    pq.offer(new Point(dis[next], next));
                 }
             }
         }
-
-        for (int i = 1; i < V + 1; i++){
-            if (dist[i] == INF){
-                System.out.println("INF");
-            } else{
-                System.out.println(dist[i]);
-            }
-        }
-
     }
+   
+    public static void main(String[] args) throws Exception {
+    	st = new StringTokenizer(br.readLine(), " ");
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(br.readLine());
+        
+        Arrays.fill(dis, 987654321);
+
+        for(int i=0;i<20002;i++) {
+        	G[i] = new ArrayList<>();
+        }
+        
+        int u,v,w;
+        for(int i=0;i<E;i++) {
+        	st = new StringTokenizer(br.readLine(), " ");
+        	u = Integer.parseInt(st.nextToken());
+        	v = Integer.parseInt(st.nextToken());
+        	w = Integer.parseInt(st.nextToken());
+        	
+        	G[u].add(new Point(v,w));
+        }
+        
+        dis[K] = 0;
+        visited[K] = true;
+        
+        BFS();
+        
+        for(int i=1;i<=V;i++) {
+        	if(dis[i] == 987654321) bw.write("INF\n");
+        	else bw.write(dis[i]+"\n");
+        }      
+
+        bw.close();
+    }    
+
 }
+
+/**
+*/
