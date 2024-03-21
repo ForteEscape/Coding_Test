472edf028a202977eedf09d4bb108ee51b6a1e9b
sehunkim <sehun5515@naver.com>
2024-03-21 11:29:04 +0900

    [Platinum IV] Title: 에바쿰, Time: 664 ms, Memory: 156648 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Platinum/15967. 에바쿰/에바쿰.java
@@ -0,0 +1,133 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+	
+	static long[] tree;
+	static long[] lazy;
+	static int[] arr;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int Q1 = Integer.parseInt(st.nextToken());
+		int Q2 = Integer.parseInt(st.nextToken());
+		
+		arr = new int[N + 1];
+		tree = new long[4 * N];
+		lazy = new long[4 * N];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 1; i <= N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		makeTree(1, 1, N);
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i = 0; i < Q1 + Q2; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int opt = Integer.parseInt(st.nextToken());
+			
+			if(opt == 2) {
+				int l = Integer.parseInt(st.nextToken());
+				int r = Integer.parseInt(st.nextToken());
+				int val = Integer.parseInt(st.nextToken());
+				
+				if(r < l) {
+					int tmp = l;
+					l = r;
+					r = tmp;
+				}
+				
+				update(1, 1, N, l, r, val);
+			} else {
+				int l = Integer.parseInt(st.nextToken());
+				int r = Integer.parseInt(st.nextToken());
+				
+				if(r < l) {
+					int tmp = l;
+					l = r;
+					r = tmp;
+				}
+				
+				long result = query(1, 1, N, l, r);
+				
+				sb.append(result).append("\n");
+			}
+		}
+		
+		System.out.print(sb);
+	}
+	
+	static void makeTree(int node, int start, int end) {
+		if(start == end) {
+			tree[node] = arr[start];
+			return;
+		}
+		
+		int mid = (start + end) >> 1;
+		makeTree(node << 1, start, mid);
+		makeTree(node << 1 | 1, mid + 1, end);
+		
+		tree[node] = tree[node << 1] + tree[node << 1 | 1];
+	}
+	
+	static void updateLazy(int node, int start, int end) {
+		if(lazy[node] != 0) {
+			tree[node] += (end - start + 1) * lazy[node];
+			
+			if(start != end) {
+				lazy[node << 1] += lazy[node];
+				lazy[node << 1 | 1] += lazy[node];
+			}
+			
+			lazy[node] = 0;
+		}
+	}
+	
+	static void update(int node, int start, int end, int left, int right, int val) {
+		updateLazy(node, start, end);
+		
+		if(start > right || end < left) {
+			return;
+		}
+		
+		if(start >= left && end <= right) {
+			tree[node] += ((long)end - start + 1) * val;
+			
+			if(start != end) {
+				lazy[node << 1] += val;
+				lazy[node << 1 | 1] += val;
+			}
+			return;
+		}
+		
+		int mid = (start + end) >> 1;
+				
+		update(node << 1, start, mid, left, right, val);
+		update(node << 1 | 1, mid + 1, end, left, right, val);
+		
+		tree[node] = tree[node << 1] + tree[node << 1 | 1];
+	}
+	
+	static long query(int node, int start, int end, int left, int right) {
+		updateLazy(node, start, end);
+		
+		if(start > right || end < left) {
+			return 0;
+		}
+		
+		if(start >= left && end <= right) {
+			return tree[node];
+		}
+		
+		int mid = (start + end) >> 1;
+		
+		return query(node << 1, start, mid, left, right) + 
+				query(node << 1 | 1, mid + 1, end, left, right);
+	}
+}
