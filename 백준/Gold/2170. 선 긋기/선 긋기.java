9946f81fd8568ea4ab3448eaabab7f07abd1e016
sehunkim <sehun5515@naver.com>
2024-04-29 17:28:27 +0900

    [Gold V] Title: 선 긋기, Time: 2604 ms, Memory: 322264 KB -BaekjoonHub

--- 백준/Gold/2170. 선 긋기/선 긋기.java
+++ 백준/Gold/2170. 선 긋기/선 긋기.java
@@ -3,23 +3,7 @@ import java.io.*;
 
 public class Main {
 	
-	static class Line implements Comparable<Line>{
-		int start;
-		int end;
-		
-		Line(int start, int end) {
-			this.start = start;
-			this.end = end;
-		}
-
-		@Override
-		public int compareTo(Line o) {
-			// TODO Auto-generated method stub
-			return this.start == o.start ? o.end - this.end : this.start - o.start;
-		}
-	}
-	
-	static Line[] line;
+	static int[][] line;
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,20 +11,21 @@ public class Main {
 		
 		int N = Integer.parseInt(br.readLine());
 		
-		line = new Line[N];
+		line = new int[N][2];
+		
 		for(int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			
 			int start = Integer.parseInt(st.nextToken());
 			int end = Integer.parseInt(st.nextToken());
 			
-			line[i] = new Line(start, end);
+			line[i] = new int[] {start, end};
 		}
 		
-		Arrays.sort(line);
+		Arrays.sort(line, (o1, o2) -> o1[0] - o2[0] == 0 ? o2[1] - o1[1] : o1[0] - o2[0]);
 		
-		int start = line[0].start;
-		int end = line[0].end;
+		int start = line[0][0];
+		int end = line[0][1];
 		
 		long ans = 0;
 		
@@ -48,16 +33,16 @@ public class Main {
 		int prevEnd = 0;
 		
 		for(int i = 1; i < N; i++) {
-			if(line[i].start <= end) {
-				end = Math.max(end, line[i].end);
+			if(line[i][0] <= end) {
+				end = Math.max(end, line[i][1]);
 			} else {
 				ans += (end - start);
 				
 				prevStart = start;
 				prevEnd = end;
 				
-				start = line[i].start;
-				end = line[i].end;
+				start = line[i][0];
+				end = line[i][1];
 			}
 		}
 		
