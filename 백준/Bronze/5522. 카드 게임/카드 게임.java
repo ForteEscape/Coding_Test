78997917e873be52f8f86da68d2b75d0098a975b
sehunkim <sehun5515@naver.com>
2024-05-13 01:03:40 +0900

    [Bronze V] Title: 카드 게임, Time: 128 ms, Memory: 14100 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int ans = 0;
+        for(int i = 0; i < 5; i++) {
+            ans += Integer.parseInt(br.readLine());
+        }
+
+        System.out.println(ans);
+    }
+}
